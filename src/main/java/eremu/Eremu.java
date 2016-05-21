4bd1bc53cf0c9a1efdb56681a531eb94088368e8
Steven Weston <echogene.alpha@gmail.com>
2016-05-21 16:04:51 +0100

    Move examples to painter

--- src/main/java/eremu/Eremu.java
+++ src/main/java/eremu/Eremu.java
@@ -38,35 +38,4 @@ public class Eremu {
 
 		imageWindow.finish();
 	}
-
-	/**
-	 * x * cos(y) + y * cos(x)
-	 *
-	 * MathUtil.towards(
-	 *         y * y * sin(4 * y),
-	 *         x * x * sin(4 * x),
-	 *         x,
-	 *         y
-	 * )
-	 *
-	 * 4*sin(sqrt(abs(x*y))) + 2*sqrt(abs(cos(x)*cos(y))) + 0.5*(cos(8*x)+cos(8*y))
-	 *
-	 * MathUtil.towards(2*y*sin(x), 2*x*cos(y), x, y)*4
-	 *         + 2*(sin(x)+sin(y))
-	 *         + 0.5*(cos(4*x)+cos(4*y))
-	 *         + 0.2*(sin(8*x)+sin(8*y)),
-	 *
-	 * 6*sqrt(x*x + y*y)
-	 *         + 6*sin(x)*cos(y)
-	 *         + 3*cos(2*x)*sin(2*y)
-	 *         + 2*cos(6*x)*cos(6*y)
-	 *         + sin(16*x)*sin(16*y)
-	 *
-	 * MathUtil.towards(
-	 *         x*cos(PI/20) - y*sin(PI/20),
-	 *         y*cos(PI/20) + x*sin(PI/20),
-	 *         x,
-	 *         y
-	 * ),
-	 */
 }
